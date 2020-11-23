07e4f179f88bd0cf8f3f64a42a9d1504d53bb8cb
ValentinAbrivard <39702869+ValentinAbrivard@users.noreply.github.com>
2020-11-23 11:53:33 +0100

    squellette back + bcrypt

---
+++ README.md
@@ -0,0 +1,24 @@
+## URL
+```
+127.0.0.1:3000
+```
+
+## Launch docker
+```
+docker-compose up
+
+CARE : Sometimes the docker compose up end up into error 256. 
+If you get this error , restart 2 times the docker compuse up and it should work on the third time. Sorry for the disturbance.
+
+If you still get the issue contact me at : v.abrivard@ecole-ipssi.net
+```
+
+## Main Page website
+```
+Login.html
+```
+
+## Access to docker
+```
+docker exec -ti container bash
+```
---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const server = express();
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const mongoose = require('mongoose');
+const path = require('path');
+mongoose.connect('mongodb://mongo/apinodeipssi');
+const bodyParser = require('body-parser');
+server.use(bodyParser.urlencoded());
+server.use(bodyParser.json());
+
+const cors = require('cors');
+server.use(cors());
+
+server.get('/', function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+})
+
+
+server.listen(port, hostname);
