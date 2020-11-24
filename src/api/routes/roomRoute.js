51a8fa9d10df75ed673aa2cb339f3a1650293aee
Empereur.j <jean.empereur@outlook.fr>
2020-11-24 17:01:49 +0100

    back-update

--- src/api/routes/roomRoute.js
+++ src/api/routes/roomRoute.js
@@ -2,7 +2,19 @@ module.exports = (server) => {
     const roomController = require('../controllers/roomController');
 
     server.route('/room')
-    .get(roomController.list_all_rooms)
-    .post(roomController.create_a_room);
+    .get(roomController.list_all_rooms) //ok
+    .post(roomController.create_a_room); //ok
+
+    server.route('/room/:room_id')
+    .get(roomController.get_a_room) //ok
+    .post(roomController.change_name_room) //ok
+    .delete(roomController.delete_a_room); //ok
+
+    server.route('/room/:room_id/live')
+    .post(roomController.live_room); //ok
+
+    server.route('/room/:room_id/:user_id')
+    .post(roomController.add_user_in_room) //ok
+    .delete(roomController.delete_user_in_room); //ok
 
 }
--- src/api/routes/userRoute.js
+++ src/api/routes/userRoute.js
@@ -2,10 +2,21 @@ module.exports = (server) => {
     const userController = require('../controllers/userController');
 
     server.route('/user')
-    .get(userController.list_all_users)
-    .post(userController.create_a_user);
+    .get(userController.list_all_users) //ok
+    .post(userController.create_a_user); //ok
+
+    server.route('/user/:user_id')
+    .get(userController.get_a_user) //ok
+    .delete(userController.delete_a_user); //ok
+
+    server.route('/user/:user_id/modify')
+    .post(userController.update_user); //ok
+
+    server.route('/user/token')
+    .post(userController.create_a_token_from_user); //ok
 
 
-    server.post('/user/login', userController.user_login)
+    server.route('/user/login')
+    .post(userController.user_login); //ok
 
 }
