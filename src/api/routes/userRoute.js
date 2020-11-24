5297288646f207c38f11588018b9d2ff8e55e2e5
Empereur.j <jean.empereur@outlook.fr>
2020-11-24 09:44:30 +0100

    Ajout JWT
    Ajout du JWT dans le userController et userRoute

--- src/api/routes/userRoute.js
+++ src/api/routes/userRoute.js
@@ -5,6 +5,9 @@ module.exports = (server) => {
     .get(userController.list_all_users)
     .post(userController.create_a_user);
 
+    server.route('/user/token')
+    .post(userController.create_a_token_from_user);
+
 
     server.post('/user/login', userController.user_login)
 
